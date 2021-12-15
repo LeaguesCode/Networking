069f028f1b6b8b64512f95db64dea7f94a8fb4fa
fangrui <fangruiwork@163.com>
2021-12-15 10:48:43 +0800

    first commit

---
+++ Networking/Classes/IJNetworkingHandler.h
@@ -0,0 +1,18 @@
+//
+//  IJNetworkingHandler.h
+//  Networking
+//
+//  Created by 方锐 on 2021/12/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IJNetworkingHandler : NSObject
+
++ (void)getDate;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Networking/Classes/IJNetworkingHandler.m
@@ -0,0 +1,16 @@
+//
+//  IJNetworkingHandler.m
+//  Networking
+//
+//  Created by 方锐 on 2021/12/15.
+//
+
+#import "IJNetworkingHandler.h"
+
+@implementation IJNetworkingHandler
+
++ (void)getDate {
+    NSLog(@"[NSDate date]");
+}
+
+@end
